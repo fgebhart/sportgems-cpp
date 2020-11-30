923142dac67325481bea72206eb7e4348dbabd33
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-11-30 18:47:16 +0000

    implement dummy class with header files and working cmake build

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "../include/Date.h"
+
 int main() {
     std::cout << "Hello World!" << "\n";
+
+    Date myDate(2020, 1, 1);
+    std::cout << "mydate day: " << myDate.getDay() << std::endl;
     return 0;
 }
